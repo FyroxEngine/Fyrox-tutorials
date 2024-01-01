daf7c74126062e8ac1944578eacedd162a627a92
Dmitry <d1maxa@yandex.ru>
2024-01-01 17:40:18 +0300

    removed rpg and platformer tutorials
    - they were moved to the book - https://github.com/fyrox-book/fyrox-book.github.io/tree/main/src/code/tutorials/platformer and https://github.com/fyrox-book/fyrox-book.github.io/tree/main/src/code/tutorials/rpg

--- platformer/executor/src/main.rs
+++
@@ -1,9 +0,0 @@
-//! Executor with your game connected to it as a plugin.
-use fyrox::engine::executor::Executor;
-use platformer::GameConstructor;
-
-fn main() {
-    let mut executor = Executor::new();
-    executor.add_plugin_constructor(GameConstructor);
-    executor.run()
-}
--- rpg-tutorial1-character-controller/executor-android/README.md
+++
@@ -1,5 +0,0 @@
-## Android Build Instructions
-
-- `cargo-apk apk run --target=armv7-linux-androideabi`
-
-TODO: Add more detailed instructions.
